c8e6008252a1ef128139a6c48ff7e57796fc2279
Stephen Brown <stephenrbrownnb@gmail.com>
2023-02-07 15:11:22 -0400

    Updated Models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,33 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        type: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        type: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
   },
   {
     sequelize,
